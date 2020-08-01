dee8079ebf7be5aba9f3fdfde4be035d799f8560
Afzal Chishti <afzal.chishti@ibm.com>
2020-08-01 19:56:22 -0400

    Populating data in about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,62 @@
 import React from "react"
 import Navbar from "../components/navbar"
 import Footer from "../components/footer"
+import Carousel from "../components/carousel";
 
 export default () => (
   <div>
     <Navbar/>
+    <div className="section first">
+      <div className="text left">
+        <h2>Who Am I?</h2>
+        <h4>
+
+        </h4>
+      </div>
+      <div className="gallery">
+        {/* <img src="/images/gatsby-icon.png" className="gallery-image"></img> */}
+        <Carousel images={["/images/pang.jpg", "/images/gatsby-icon.png","/images/ds.jpg"]} description={["a", "b", "c"]}/>
+      </div>
+    </div>
+
+    <div className="section-alt">
+      <div className="text right">
+
+      </div>
+      <div className="gallery"> 
+
+      </div>
+    </div>
+
+    <div className="section last">
+      <div className="text left">
+        <h2>Interests</h2>
+        <h4>
+          I have numerous interests such as 
+          <ul>
+            <li>Machine Learning</li>
+            <li>Web Development</li>
+            <li>Data Science</li>
+            <li>Game Development</li>
+            <li>App Development</li>
+          </ul>
+        </h4>
+      </div>
+      <div className="gallery">
+        {/* <img src="/images/gatsby-icon.png" className="gallery-image"></img> */}
+        <Carousel images={["/images/pang.jpg", "/images/gatsby-icon.png","/images/ds.jpg"]} description={["a", "b", "c"]}/>
+      </div>
+    </div>
+
+
+
     <div className="content">
       <h1>Future Goals</h1>
       <h3>My future goals at the moment would be to graduate in the immediate future, before working in the software industry. Another goal would be to create my own YouTube channel in which I create tutorials and tips for others in the industry. A goal for me would be to teach either at a conference or </h3>
       <h1>Interests</h1>
       <h2>Programming</h2>
       <ul>
-        <li>Machine Learning</li>
+        <li></li>
         <li>Web Development</li>
         <li>Data Science</li>
         <li>Game Development</li>
@@ -23,11 +68,7 @@ export default () => (
         <li>TV (huge fan of anime)</li>
       </ul>
 
-      <h1>Work</h1>
-      <h3>I am currently working as a devops engineer for IBM Security Guardium Insights. My role entails incorporating other technologies with the product. Recently, I was in charge of incorporating IBM Db2 warehouse with the product for the previous release. 
-        
-        In the the upcoming release, I'm in charge of utilizing cert manager which is a Kubernetes add-on that automates the management and issuance of TLS certificates to create all the TLS certificates for IBM Security Guardium Insights.</h3>
-    </div>
+      </div>
     <Footer/>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const IndexPage = () => (
       
       
       <div className="section first">
-        <div className="text">
+        <div className="text left">
           <h2>What Am I Doing?</h2>
           <h4>
             I'm a DevOps engineer as of this moment working on IBM Guardium Insights, soon to become a final year computer science student in around a month. <br />
@@ -34,7 +34,23 @@ const IndexPage = () => (
 
       </div>
 
-      <div className="section last"></div>
+      <div className="section last">
+        <div className="text left">
+          <h2>What Am I Doing?</h2>
+          <h4>
+            I'm a DevOps engineer as of this moment working on IBM Guardium Insights, soon to become a final year computer science student in around a month. <br />
+            Officially, my job is to bridge the gap between development code and production level code by ensuring that the development code works in a production environment. I also work with third-party products to incorporate into our product.
+            <br />
+            It's all about making the deadlines as stress-free as possible by finding issues in the code and bringing these issues up with the developers, so that the issues can be dealt with promptly.
+            <br />
+            If developers are to be considered as chefs, my role could be described as a head chef as I approve and polish the dishes(code) before they reach the customer. 
+          </h4>
+        </div>
+        <div className="gallery">
+          {/* <img src="/images/gatsby-icon.png" className="gallery-image"></img> */}
+          <Carousel images={["/images/pang.jpg", "/images/gatsby-icon.png","/images/ds.jpg"]} description={["a", "b", "c"]}/>
+        </div>
+      </div>
 
     <Footer/>
   </div>
